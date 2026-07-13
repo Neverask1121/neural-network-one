6d2dac7cbe2a41020c85c0c780cfd1da478d176d
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-07-13 04:09:02 +0000

    enabling the app for visualization in streamlit

---
+++ app.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import streamlit as st
+import numpy as np
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pandas as pd
+import pickle
+
+## Loading the trained model
+
